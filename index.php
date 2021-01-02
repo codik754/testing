48a5e7645ec3586a3ac5606c148d5064f55e9883
codik754 <5ochta@bk.ru>
2021-01-02 23:06:57 +0700

    Обновление в 23:07

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
+  echo "Work";
+  echo "Work";
+  echo "Work";
+  echo "Work";
+  echo "Work";
+  echo "Work";
 <?php
+  echo "Work123";
   echo "Work";
   echo "Work1";
   echo "New text";
@@ -7,3 +14,9 @@
   echo "New text";
   echo "New text";
 ?>
+  echo "Work";
+  echo "Work";
+  echo "Work";
+  echo "Work";
+  echo "Work";
+  echo "Work";
