22db26914c155d9f6ccfd22e381fdcff6c0fe7b9
codik754 <5ochta@bk.ru>
2021-01-03 18:28:29 +0700

    Настроил .htaccess

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-   $var1 = "Work123";
-   var_dump($var1);
+<?php 
+    if (isset($_GET['page'])) {
+	$requested_page = $_GET['page'];
+	}
+	else {
+	$requested_page = 'index';
+	}
+    echo "Вы переходите на: <div style='color:green;display:inline;'> ".$requested_page."</div>";
+// a better way would be to put this into an array, but I think a switch is easier to read for this example
+/*switch($requested_page) {
+   case "blog":
+      include(__DIR__."/blog.php");
+      break;
+   case "home":
+      include(__DIR__."/home.php");
+      break;
+   default:
+      include(__DIR__."/404.php");*/
 ?>
+
