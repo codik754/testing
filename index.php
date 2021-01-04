06b88ad07c0869239fd4a1160db2882fad8b235a
codik754 <5ochta@bk.ru>
2021-01-04 11:29:09 +0700

    Это нужно было сделать

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<?php 
-    if (isset($_GET['page'])) {
-	$requested_page = $_GET['page'];
-	}
-	else {
-	$requested_page = 'index';
-	}
-    echo "Вы переходите на: <div style='color:green;display:inline;'> ".$requested_page."</div>";
-// a better way would be to put this into an array, but I think a switch is easier to read for this example
-/*switch($requested_page) {
-   case "blog":
-      include(__DIR__."/blog.php");
-      break;
-   case "home":
-      include(__DIR__."/home.php");
-      break;
-   default:
-      include(__DIR__."/404.php");*/
+<?php
+  error_reporting(E_ALL);
+  echo "Work!";  
 ?>
-
