688dd9e79a2dfea097a9720057db7a12c8782903
codik754 <5ochta@bk.ru>
2021-01-05 11:21:31 +0700

    Вопрос про прототип

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
    class classObj{
       public $var = 55;
 
-      function classObj($var1);
+      function classObj($var1);//ТАК возможно сделать
 
       function show(){
          echo "Value: ".$this->var;
       }
    }
 
+   //ТАК ВОЗМОЖНО КАК_ТО СДЕЛАТЬ
    classObj::classObj($var1){
       $this->var = $var1;
    }
