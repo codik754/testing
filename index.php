a2edea5adef2e2125c57ff9c231c36bd46b5e945
codik754 <5ochta@bk.ru>
2021-01-05 11:18:10 +0700

    Вопрос про прототип функции

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-  error_reporting(E_ALL);
-  echo "Work!";  
+   class classObj{
+      public $var = 55;
+
+      function classObj($var1);
+
+      function show(){
+         echo "Value: ".$this->var;
+      }
+   }
+
+   classObj::classObj($var1){
+      $this->var = $var1;
+   }
+
+   $Obj = new classObj(10);
+   $Obj->show(); 
+
+   var_dump($Obj);
 ?>
